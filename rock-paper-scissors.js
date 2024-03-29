b37e5df6af87386905ac318e699807fc3b01339e
Tevis <taspicknall@gmail.com>
2024-03-29 05:35:33 -0400

    Test getComputerChoice() start to write game func

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,13 +1,24 @@
 function getComputerChoice() {
     let choice = Math.random();
-    // Math.random() generates a function between 1 and 0
+    // Math.random() generates a function between 1 and 0.
     if (choice <= 0.333) {
         choice = "rock";
     } else if (choice <= 0.666) {
         choice = "paper";
     } else {
-        choice = "scissors"
-    }
+        choice = "scissors";
+    } 
+    console.log(choice)
 }
-console.log(getComputerChoice(choice))
-// How do I use console.log()!!??
+
+getComputerChoice();
+// test that getComputerChoice(); works... it does!
+
+function playRound(playerSelection, computerSelection) {
+//need to figure out code for step 4 on TOP here
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
