67031563289d2d28a7972317c4647af2b2fccdc1
Tevis <taspicknall@gmail.com>
2024-04-03 06:16:01 -0400

    Add playRound func, manipulate to get to work

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,22 +3,46 @@ function getComputerChoice() {
     // Math.random() generates a function between 1 and 0.
     if (choice <= 0.333) {
         choice = "rock";
+        return "rock";
     } else if (choice <= 0.666) {
         choice = "paper";
-    } else {
+        return "paper";
+    } else if (choice <= 1) {
         choice = "scissors";
-    } 
-    console.log(choice)
+        return "scissors";
+    }   
 }
-
-getComputerChoice();
-// test that getComputerChoice(); works... it does!
 
 function playRound(playerSelection, computerSelection) {
-//need to figure out code for step 4 on TOP here
+// structure for outcomes processed in game.
 
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Rock smashes Scissors, you win!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Scissors cuts Paper, you win!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Paper covers Rock, you win!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "Paper covers Rock, you lose";
+    }
+    else if (playerSelection === "scisssors" && computerSelection === "rock") {
+        return "Rock smashes Scissors, you lose.";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "Scissors cuts Paper, you lose.";
+    }
+    else if (playerSelection === computerSelection) {
+        return "tie";
+    }   
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    const playerSelection = "RoCk";
+
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection)); 
+    console.log(getComputerChoice());
+    console.log(playerSelection);
+    console.log(computerSelection);
