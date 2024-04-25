db2c548a8aafbdf64ad48367035ec8a22edc8cfb
Tevis <taspicknall@gmail.com>
2024-04-25 05:51:47 -0400

    Change parameter name in gameResult()
    Change name so that they are unique and not the same
    as previously declared variables.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -16,34 +16,34 @@ function getComputerChoice() {
 }
 
  // buckets for playRound results to be stored
-let playerScore = 0;
-let computerScore = 0;
+let playerWins = 0;
+let computerWins = 0;
 let tie = 0;
 
 // structure for outcomes processed in game.
 function playRound(playerSelection, computerSelection) {     
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore++;
+        playerWins++;
         return "Rock smashes Scissors, you win!";
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore ++;
+        playerWins ++;
         return "Scissors cuts Paper, you win!";
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore ++;
+        playerWins ++;
         return "Paper covers Rock, you win!";
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore ++;
+        computerWins ++;
         return "Rock is covered by Paper, you lose";
     }
     else if (playerSelection === "scisssors" && computerSelection === "rock") {
-        computerScore ++;
+        computerWins ++;
         return "Scissors are smashed by Rock, you lose.";
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore ++;
+        computerWins ++;
         return "Paper is cut by Scissors, you lose.";
     }
     else if (playerSelection === computerSelection) {
@@ -68,21 +68,20 @@ function playGame() {
         console.log (playRound(playerInput(), computerSelection)) 
      }
     }
+playGame()
 
-function gameResults(playerScore,computerScore) { 
-    if (playerScore > computerScore) {
+function gameResults(playerWins,computerWins) { 
+    if (playerWins > computerWins) {
         return "You won more rounds than the computer. You win!!!";
     }
-    else if (playerScore < computerScore) {
-        return "The Computer woplayerScore, computerScoren more rounds. You lose.";
+    else if (playerWins < computerWins) {
+        return "The Computer won more rounds. You lose.";
     }
-    else if (playerScore == computerScore) {
-        return "You and for computer won an equal amount of games. It's a tie.";
-    }
-    console.log(gameResults(playerScore, computerScore))   
+    else if (playerWins === computerWins) {
+        return "You and for computer won an equal number of games. It's a tie.";
+    }  console.log(gameResults(playerScore, computerScore))
 }
 
-playGame()
 gameResults()
 
 
